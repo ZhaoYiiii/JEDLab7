020446fe6bb40bdd482eab6d024855d7669738ca
Abdul Hadi <Abdul Hadi@DESKTOP-GKAN98N>
2020-07-01 13:57:48 +0800

    First Import

---
+++ src/java/sg/edu/nyp/RegistrationServlet.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sg.edu.nyp;
+
+import java.io.IOException;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Abdul Hadi
+ */
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+     //Inject your EJB here
+    
+    @EJB 
+   
+    
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        //Get the name parameter into a local variable
+        String name = request.getParameter("name");
+       
+    }
+}
