e4032ecc81bf5ebcea7ef52f1ac83cd496c220f2
Kimberlykwok <183509QQ@mymail.nyp.edu.sg>
2020-07-06 15:25:50 +0800

    Checkout summary

--- src/java/sg/edu/nyp/Item.java
+++ src/java/sg/edu/nyp/Item.java
@@ -6,11 +6,56 @@
 
 package sg.edu.nyp;
 
+import java.io.Serializable;
+
 /**
  *
  * @author fepit
  */
-public class Item {
+public class Item implements Serializable{
+    
+    private final long serialVersionUID = -1L;
+    
+    private int itemId;
+    private int categoryId;
+    private String itemName;
+    private Float price;
+
+    public Item() {
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+    
     
     
 }
